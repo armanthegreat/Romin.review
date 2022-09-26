ed9bc56793aabb378ebb4f0c9141960bdeaa8604
arman.valanejad <arman.valanejad@triply.cc>
2022-09-26 20:05:26 +0200

    `

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// Reference of the paragraph tag
-const content = document.getElementById("content");
 
-// Reference of the copy button
-const copyBtn = document.getElementById("copy");
-
-// Copy button's onclick handler
-copyBtn.onclick = copyToClipboard;
-
-// Method responsible for copying the content
-function copyToClipboard() {
-    navigator.clipboard
-        .writeText(content.innerText);
-        location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";
-}
-
-const pasteBtn = document.getElementById("paste");
-
-
-
+    window.onload = (event) => {
+        navigator.clipboard
+        .writeText("Had a great time. Huge thanks to Romin and Aldo");
+        location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";    };
 // https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2Fpages%2Fcategory%2FBoat-Tour-Agency%2FStarboard-Boats-Amsterdam-301283120419739%2F%3Flocale%3Dfr_FR%26_rdr&psig=AOvVaw0VWhnIOTKuENPLVWyLbNQT&ust=1664233412725000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCQmOCGsfoCFQAAAAAdAAAAABAD
