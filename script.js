148953c33f93d4c1b8b704eda4ddafbbcfc6badd
arman.valanejad <arman.valanejad@triply.cc>
2022-09-26 12:16:26 +0200

    Now only one button

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ copyBtn.onclick = copyToClipboard;
 function copyToClipboard() {
     navigator.clipboard
         .writeText(content.innerText);
+        location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";
 }
 
 const pasteBtn = document.getElementById("paste");
 
-pasteBtn.onclick = function () {
-    location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";
-} ;
+
 
 // https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2Fpages%2Fcategory%2FBoat-Tour-Agency%2FStarboard-Boats-Amsterdam-301283120419739%2F%3Flocale%3Dfr_FR%26_rdr&psig=AOvVaw0VWhnIOTKuENPLVWyLbNQT&ust=1664233412725000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCQmOCGsfoCFQAAAAAdAAAAABAD
