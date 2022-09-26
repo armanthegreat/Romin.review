850f56f1616b6d95eb550ac8cfb6c9070964a4cf
arman.valanejad <arman.valanejad@triply.cc>
2022-09-26 20:30:59 +0200

    Bug fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
  window.onload = (event) => {
         navigator.clipboard
-        .writeText("content.innerText");
+        .writeText(content.innerText);
         location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";    };
