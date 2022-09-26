32d4a42d922f1b101bd9ecfb6d41eb63a8204ef1
armanthegreat <70899947+armanthegreat@users.noreply.github.com>
2022-09-26 20:26:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-    window.onload = (event) => {
+ window.onload = (event) => {
         navigator.clipboard
-        .writeText("Had a great time. Huge thanks to Romin and Aldo");
+        .writeText("content.innerText");
         location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";    };
-// https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2Fpages%2Fcategory%2FBoat-Tour-Agency%2FStarboard-Boats-Amsterdam-301283120419739%2F%3Flocale%3Dfr_FR%26_rdr&psig=AOvVaw0VWhnIOTKuENPLVWyLbNQT&ust=1664233412725000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCQmOCGsfoCFQAAAAAdAAAAABAD
