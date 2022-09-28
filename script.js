17430edef710d4a17ad332365edcf92eee75c019
armanthegreat <70899947+armanthegreat@users.noreply.github.com>
2022-09-28 12:23:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+
+
 // Reference of the paragraph tag
 const content = document.getElementById("content");
 
-    
+// Reference of the copy button
+const copyBtn = document.getElementById("copy");
 
+// Copy button's onclick handler
+copyBtn.onclick = copyToClipboard;
 
-const pasteBtn = document.getElementById("paste");
-
-    window.onload = (event) => {
-        navigator.clipboard
-        .writeText(document.getElementById("content").innerText);
-        location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";    };
+// Method responsible for copying the content
+function copyToClipboard() {
+    navigator.clipboard
+        .writeText(content.innerText);
+        location.href = "https://search.google.com/local/writereview?placeid=ChIJqytiXK0JxkcR6RBSxZwMw3U";
+}
